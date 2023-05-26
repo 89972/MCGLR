ade3e8f181e0cd9749e6d441aa448e4fd99ee41b
89972 <121228696+89972@users.noreply.github.com>
2023-05-26 14:36:48 +0200

    done done done

---
+++ Assets/Scripts/ImageTracking.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ImageTracking : MonoBehaviour
+{
+    public string ReferenceImageName;
+    private ARTrackedImageManager _TrackedImageManager;
+
+    void Awake()
+    {
+        _TrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    void OnEnable()
+    {
+        if(_TrackedImageManager != null)
+        {
+            _TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+        }
+    }
+
+    void OnDisabled()
+    {
+        if(_TrackedImageManager != null)
+        {
+            _TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+        }
+    }
+    
+    private void OnTrackedImagesChanged(ARTrackedImagechangedEventArgs e)
+    {
+        foreach (var trackedImage in e.added)
+        {
+            Debug.Log($"Tracked image detected {trackedImage.ReferenceImageName.name} with size {trackedImage.size}");
+        }
+
+        UpdateTrackedImages(e.added);
+        UpdateTrackedImages(e.updated);
+    }
+
+        private void UpdateTrackedImages(IEnumerable<ARTrackedImage> trackedImages)
+    {
+        // If the same image (RefrenceImageName)
+         var trackedImage = trackedImage.FirstOrDefault(x => x.referenceImageName == ReferenceImageName); 
+         if (trackedImage == null)
+         {
+            return;
+         }
+      
+
+    if (trackedImage.trackingState != trackingState.None)
+    {
+        var trackedImageTransform = trackedImage.transorm;
+        transform.SetPositionAndRotation(trackedImageTransform.position, trackedImageTransform.rotation);
+    }
+    
+   
+}
+}
